821c8cec66b0e6f2cddd332b1fe221b03b371083
Justin Libert <153557622+LibertJustin@users.noreply.github.com>
2026-02-06 19:20:21 +0100

    Update storage.rs

--- src/storage.rs
+++ src/storage.rs
@@ -1,4 +1,4 @@
-use crate::models::Task; // Import Task from models
+use crate::models::Task;
 use std::path::PathBuf;
 
 fn get_db_path() -> PathBuf {
@@ -8,7 +8,7 @@ fn get_db_path() -> PathBuf {
         .expect("Could not find home directory.");
 
     let mut path = PathBuf::from(home_dir);
-    path.push(".todo_list_data.json"); // .push handles the slash automatically
+    path.push(".todo_list_data.json");
     return path;
 }
 
@@ -19,7 +19,7 @@ fn get_db_path_backup() -> PathBuf {
         .expect("Could not find home directory.");
 
     let mut path = PathBuf::from(home_dir);
-    path.push(".todo_list_data_backup.json"); // .push handles the slash automatically
+    path.push(".todo_list_data_backup.json");
     return path;
 }
 
@@ -31,28 +31,20 @@ pub fn save_tasks(todos: &Vec<Task>) {
 
 pub fn load_todos() -> Vec<Task> {
     match std::fs::read_to_string(get_db_path()) {
-        Ok(content) => {
-            // This one line parses the JSON string back into a Vec<Task>
-            // If the file is corrupted, it returns an error, which we unwrap
-            serde_json::from_str(&content).unwrap_or_else(|_| {
-                println!("File corrupted, trying backup.");
-                load_todos_backup()
-            })
-        }
+        Ok(content) => serde_json::from_str(&content).unwrap_or_else(|_| {
+            println!("File corrupted, trying backup.");
+            load_todos_backup()
+        }),
         Err(_) => Vec::new(),
     }
 }
 
 pub fn load_todos_backup() -> Vec<Task> {
     match std::fs::read_to_string(get_db_path_backup()) {
-        Ok(content) => {
-            // This one line parses the JSON string back into a Vec<Task>
-            // If the file is corrupted, it returns an error, which we unwrap
-            serde_json::from_str(&content).unwrap_or_else(|_| {
-                println!("Backup corrupted, starting fresh.");
-                Vec::new()
-            })
-        }
+        Ok(content) => serde_json::from_str(&content).unwrap_or_else(|_| {
+            println!("Backup corrupted, starting fresh.");
+            Vec::new()
+        }),
         Err(_) => Vec::new(),
     }
 }
