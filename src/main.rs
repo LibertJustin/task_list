885147a0df42997233901413112490ac0ea86fb3
Justin Libert <153557622+LibertJustin@users.noreply.github.com>
2026-02-15 09:30:33 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{CommandFactory, Parser, Subcommand};
 
+use std::process::Command;
+use std::str;
 mod models;
 use models::*;
 mod storage;
@@ -48,6 +50,18 @@ fn main() {
             delete_multiple_task(&mut todos, &id);
         }
         Commands::View => {
+            let output = if cfg!(target_os = "windows") {
+                Command::new("cmd")
+                    .args(["/C", "clear"])
+                    .output()
+                    .expect("failed to execute process")
+            } else {
+                Command::new("sh")
+                    .arg("-c")
+                    .arg("clear")
+                    .output()
+                    .expect("failed to execute process")
+            };
             show_todo(&todos);
         }
         Commands::Edit { id, task } => {
